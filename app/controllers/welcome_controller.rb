c370e1857a6a6dfa303f944625748578e35c017f
Jing <madrid26@sina.com>
2017-03-20 20:36:05 +0800

    create groups index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,7 @@
 class WelcomeController < ApplicationController
+  def index
+    flash[:notice] = "Buenos dias!"
+    flash[:alert] = "Buenas noches!"
+    flash[:warning] = "Este es warning!"
+  end
 end
